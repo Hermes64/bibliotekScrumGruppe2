131d4a342e90152127abbaa90c281b283df7ff4a
Mercurio <65070556+Hermes64@users.noreply.github.com>
2021-03-11 11:27:56 +0100

    index.html and controller

--- filialbiliotek/src/main/java/filialbiliotek/packages/Controller/homeController.java
+++ filialbiliotek/src/main/java/filialbiliotek/packages/Controller/homeController.java
@@ -1,9 +1,15 @@
 package filialbiliotek.packages.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class homeController {
+
+    @GetMapping("/")
+    public String frontPage(){
+        return "frontPage";
+    }
 
 
 
